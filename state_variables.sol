3522cfc0f77fdd5577710b6d900fca3110780cfd
Venkatesh Chowdary <68181381+iamvaar-dev@users.noreply.github.com>
2023-10-16 02:07:33 +0530

    Update state_variables.sol

--- state_variables.sol
+++ state_variables.sol
@@ -3,12 +3,12 @@
 
     contract statevariable{
 
-     //First way to intialize state variables
+     //First way to initialize state variables
        uint8 public test = 1; //state variable 1
 
 
 
-       //Second way to intialize state variables
+       //Second way to initialize state variables
        uint8 public test1; //state variable 2
        constructor() {
         test1 = 2;
@@ -16,7 +16,7 @@
         
 
 
-        //Third way to intialize state variables
+        //Third way to initialize state variables
         uint8 public test2; //state variable 3
         function allot() public {
             test2 = 3;
@@ -24,6 +24,6 @@
     }
 
 /*note:   
-1.state variables are declared inside the contract braces in contract name{}
-2. State variables costs us a lot of gas so we should use them precisely eg:if the number is unsigned small value we just use uint8(based on the requirement)
-3. These are stored on storage not on stack so they are permenantly stored on blockchain.*/
+1. state variables are declared inside the contract braces in contract name{}
+2. State variables cost us a lot of gas so we should use them precisely eg: if the number is an unsigned small value we just use uint8(based on the requirement)
+3. These are stored on storage not on the stack so they are permanently stored on the blockchain.*/
