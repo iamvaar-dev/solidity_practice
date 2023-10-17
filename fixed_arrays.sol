ade9183ab755b97840c7f696004a8d39f628153c
Iamvaar <venkibvb5192@gmail.com>
2023-10-17 22:17:42 +0530

    practiced Fiexed arrays

---
+++ fixed_arrays.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract fixedArrays{
+    uint8[5] public arr = [1,2,3,4,5];
+    function test(uint8 _index, uint8 _item) public {
+        arr[_index] = _item;
+
+    }
+
+function test1(uint8 _indexx) public view returns(uint8){
+    return arr[_indexx];
+}
+
+function wholedata() public view returns(uint8[5] memory){
+    return arr;
+}
+
+}
+
+/*
+Note:
+Arrays comes under reference data type.
+For returning reference data types we should use memory keyword in returns().
+*/
